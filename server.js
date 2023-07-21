ca10bfb7169d93f472bff7bf9bf2141dc3f89339
Yajiit <tjagerter@gmail.com>
2023-07-20 22:09:00 -0400

    added GET route. added Server startup.

--- server.js
+++ server.js
@@ -7,3 +7,22 @@ const PORT = 3001;
 
 app.use(express.json());
 app.use(express.static('public'));
+
+// // // API ROUTES // // //
+// API GET
+app.get('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed to read notes' });
+      }
+  
+      const notes = JSON.parse(data);
+      res.json(notes);
+    });
+  });
+
+//   Server startup
+app.listen(PORT, () => {
+    console.log(`Server running successfully on http://localhost:${PORT}`);
+  });
