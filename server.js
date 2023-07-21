d582c316e629b881517b7f2ebf906bef080a45f3
Yajiit <tjagerter@gmail.com>
2023-07-20 22:14:57 -0400

    added POST route for saving notes.

--- server.js
+++ server.js
@@ -21,6 +21,40 @@ app.get('/api/notes', (req, res) => {
       res.json(notes);
     });
   });
+
+  // API POST
+app.post('/api/notes', (req, res) => {
+    fs.readFile(path.join(__dirname, 'db/db.json'), 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Failed saving note' });
+      }
+  
+      const notes = JSON.parse(data);
+      const newNoteId = Date.now().toString();
+      const newNote = {
+        id: newNoteId,
+        title: req.body.title,
+        text: req.body.text,
+      };
+  
+      notes.push(newNote);
+  
+      fs.writeFile(
+        path.join(__dirname, 'db/db.json'),
+        JSON.stringify(notes),
+        'utf8',
+        (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).json({ error: 'Failed saving note' });
+          }
+  
+          res.json(newNote);
+        }
+      );
+    });
+  });
   
 // Serve the HTML
 app.get('/notes', (req, res) => {
